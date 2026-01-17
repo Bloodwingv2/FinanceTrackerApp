804cded32d5c350e0f9c89535e6b6d3ab8ae2ac7
Bloodwingv2 <bhandariumesh81@gmail.com>
2026-01-17 01:42:38 +0100

    Fixed all export errors moving to mordernization

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import {
 } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as db from '../db';
 
 type TransactionType = 'expense' | 'income';
